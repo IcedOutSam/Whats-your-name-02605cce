8990ad33fbc776505effa502c1268d8b5d630564
sam <2102926@talnet.nl>
2020-01-28 13:21:16 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+ echo ("Wat is uw voornaam?".PHP_EOL);
+ $line1 = readline("".PHP_EOL);
+ echo ("Wat is uw achternaam?".PHP_EOL);
+ $line2 = readline("".PHP_EOL);
+ echo ("Uw naam  is: " . $line1 . " " . $line2);
